7cedda24221524cfdbf4ce88866e2d9237bc928f
Adgar <YOUR EMAIL@SAMLECOM>
2025-02-05 20:50:27 +0300

    добавил обработку ошибок

--- modules/handlePostClick.js
+++ modules/handlePostClick.js
@@ -3,39 +3,17 @@ import { fetchRenderComment } from "./modules/fetchRenderAndLike.js"
 
 let input = document.querySelector(".add-form-name")
 let textInput = document.querySelector(".add-form-text")
-let btn = document.querySelector(".add-form-button")
 let addForm = document.querySelector(".add-form")
 let loaderForm = document.querySelector(".loaderBox")
-let loaderComm = document.querySelector(".loaderComments")
 
-// let counter = 0
-
-// let options = { hour: "2-digit", minute: "2-digit" }
-// let optDate = { year: "2-digit", month: "2-digit", day: "2-digit" }
-
-fetchRenderComment()
-
-loaderComm.style.display = "none"
-
-btn.addEventListener("click", function () {
-    let newLikeArr = {
-        // date:
-        //     new Date().toLocaleDateString("ru-RU", optDate) +
-        //     " " +
-        //     new Date().toLocaleTimeString("ru-RU", options),
-        // likes: counter,
-        // isLiked: false,
-        text: sanitizeHtml(textInput.value),
-        name: sanitizeHtml(input.value),
-        forceError: true,
-    }
-    addForm.classList.add("add-form-opacity")
-    loaderForm.textContent = "Комментарий добавляется..."
+export const handlePostClick = (Arr) => {
     fetch("https://wedev-api.sky.pro/api/v1/adgar-gajfullin/comments", {
         method: "POST",
-        body: JSON.stringify(newLikeArr),
+        body: JSON.stringify(Arr),
     })
         .then((response) => {
+            console.log(response.status)
+
             if (response.status === 400) {
                 throw new Error("Введены некоректные данные")
             }
@@ -74,6 +52,4 @@ btn.addEventListener("click", function () {
             addForm.classList.remove("add-form-opacity")
             loaderForm.style.display = "none"
         })
-
-    // likesArr.push(newLikeArr)
-})
+}
